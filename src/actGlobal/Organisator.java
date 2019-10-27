f88c062f2a13ea9560a229b36992d5e6e7d0c1ec
HuePhung <56964065+HuePhung@users.noreply.github.com>
2019-10-27 18:44:48 +0100

    organisator erhält arrayList aktionsliste + Getter&Setter

--- src/actGlobal/Organisator.java
+++ src/actGlobal/Organisator.java
@@ -1,46 +1,70 @@
 package actGlobal;
 
-public class Organisator {
+import java.util.ArrayList;
 
-	private String email;
+public class Organisator {
+	
+	//Hier werden die Eingabeforderungen im String gespeichert
+	private String email;  
 	private String nachname;
 	private String vorname;
 	private String passwort;
+	
+	// mithilfe der folgendenen Zuweisung 
+	private ArrayList<Aktion> aktionsListe = new ArrayList<Aktion>();
+	
+	
+	public Organisator(String email, String nachname, String vorname, String passwort) { 
+		
+		// parametrisierter Konstruktor
+		this.setEmail(email);
+		this.setNachname(nachname);
+		this.setVorname(vorname);
+		this.setPasswort(passwort);
 
-	public Organisator() {
-
+	}
+	
+	// Getter&Setter fuer die privatisierten Attribute
+	
+	
+	
+	public ArrayList<Aktion> getAktionsListe() {
+		return aktionsListe;
+	}
+	public void setAktionsListe(ArrayList<Aktion> aktionsListe) {
+		this.aktionsListe = aktionsListe;
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public String setEmail(String email) {
-		return this.email = email;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getNachname() {
 		return nachname;
 	}
 
-	public String setNachname(String nachname) {
-		return this.nachname = nachname;
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
 	}
 
 	public String getVorname() {
 		return vorname;
 	}
 
-	public String setVorname(String vorname) {
-		return this.vorname = vorname;
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
 	}
 
 	public String getPasswort() {
 		return passwort;
 	}
 
-	public String setPasswort(String passwort) {
-		return this.passwort = passwort;
+	public void setPasswort(String passwort) {
+		this.passwort = passwort;
 	}
 
 }
