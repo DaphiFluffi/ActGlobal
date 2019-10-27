fc1ed07a39e1c090bb6b01616a42d501ea4b4490
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2019-10-27 19:09:26 +0100

    Gehlermeldung bei falscher Konstruktor Übergabe eingefügt und mehr Kommentare

--- src/actGlobal/Spende.java
+++ src/actGlobal/Spende.java
@@ -7,23 +7,39 @@ public class Spende {
 	private boolean quittung;
 	private String spendername;
 	private Status status;
+	//jede Spende muss genau ein Konto und genau eine Aktion haben
+	//deshalb benoetigt die Klasse Spende die entsprechenden Klassenattribute
 	private Aktion aktion;
 	private Konto konto;
 
 	
 	//parametrisierter Konstruktor 
+	//da genau ein Konto und genau eine Aktion pro Spende existieren muss, werden die Klassenvariablen in den Konstruktor als Parameter uebergeben
+	//so kann kein Objekt der Klasse Spende instanziiert werden, ohne ein Konto und eine Aktion angegeben zu haben 
 	public Spende(double betrag, boolean quittung, String spendername, Status status, Konto konto, Aktion aktion) {
 		this.betrag = betrag;
 		this.quittung = quittung;
 		this.spendername = spendername;
 		this.status = Status.IN_BEARBEITUNG; //als default wert?
+		
+		// Wenn statt eines richtigen Konto- oder Aktion-Objektes null übergeben wurde, soll diese Fehlermeldung erscheinen
+		if(konto == null ) {
+			System.out.println("Die Angabe des Kontos fehlt. Bitte nochmal versuchen.");
+			System.exit(0);
+		}
+		
+		if(aktion == null ) {
+			System.out.println("Die Angabe der Aktion fehlt. Bitte nochmal versuchen.");
+			System.exit(0);
+		}
+		
 		this.konto = konto;
 		this.aktion = aktion;
 	}
 
 	//getter Methoden fuer die private Klassenattribute
 	public double getBetrag() {
-		return betrag; //this? 
+		return betrag; 
 	}
 
 	public boolean getQuittung() {
@@ -37,8 +53,17 @@ public class Spende {
 	public Status getStatus() {
 		return this.status;
 	}
+	
+	public Aktion getAktion() {
+		return aktion;
+	}
+	
+	public Konto getKonto() {
+		return konto;
+	}
 
 	//setter Methoden die private Klassenattribute
+	
 	public void setBetrag(double betrag) {
 		this.betrag = betrag;
 	}
@@ -51,17 +76,11 @@ public class Spende {
 		this.spendername = spendername;
 	}
 
-	public Aktion getAktion() {
-		return aktion;
-	}
 
 	public void setAktion(Aktion aktion) {
 		this.aktion = aktion;
 	}
 
-	public Konto getKonto() {
-		return konto;
-	}
 
 	public void setKonto(Konto konto) {
 		this.konto = konto;
