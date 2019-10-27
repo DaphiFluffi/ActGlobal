bede575ed072e673869701d40d20b3a9eafc7fec
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2019-10-27 18:35:58 +0100

    Hinzufuegen von Klassenvaribale Aktion und Konto mit getter setter und Einfuegen in den Konstruktor

--- src/actGlobal/Spende.java
+++ src/actGlobal/Spende.java
@@ -2,21 +2,27 @@ package actGlobal;
 
 public class Spende {
 
+	//Klassenvariablen fuer Betrag, Quittung, Spendername und Status anlegen
 	private double betrag;
 	private boolean quittung;
 	private String spendername;
 	private Status status;
-
-	public Spende(double betrag, boolean quittung, String spendername, Status s) {
+	private Aktion aktion;
+	private Konto konto;
+	
+	//parametrisierter Konstruktor 
+	public Spende(double betrag, boolean quittung, String spendername, Status status, Konto konto, Aktion aktion) {
 		this.betrag = betrag;
 		this.quittung = quittung;
 		this.spendername = spendername;
-		status = Status.IN_BEARBEITUNG; //vielleicht dafür auch noch einen Getter Setter?
+		this.status = Status.IN_BEARBEITUNG; //als default wert?
+		this.konto = konto;
+		this.aktion = aktion;
 	}
 
-
+	//getter Methoden fuer die private Klassenattribute
 	public double getBetrag() {
-		return betrag;
+		return betrag; //this? 
 	}
 
 	public boolean getQuittung() {
@@ -26,7 +32,12 @@ public class Spende {
 	public String getSpendername() {
 		return spendername;
 	}
+	
+	public Status getStatus() {
+		return this.status;
+	}
 
+	//setter Methoden die private Klassenattribute
 	public void setBetrag(double betrag) {
 		this.betrag = betrag;
 	}
@@ -38,4 +49,22 @@ public class Spende {
 	public void setSpendername(String spendername) {
 		this.spendername = spendername;
 	}
+
+	public Aktion getAktion() {
+		return aktion;
+	}
+
+	public void setAktion(Aktion aktion) {
+		this.aktion = aktion;
+	}
+
+	public Konto getKonto() {
+		return konto;
+	}
+
+	public void setKonto(Konto konto) {
+		this.konto = konto;
+	}
+	
+	//readonly? keine setter methode für Status
 }
